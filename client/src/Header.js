204bea66701e7df18813361671c26b0885a610a9
aashiasorathia <aashiasorathiya@gmail.com>
2025-07-21 23:12:08 +0530

    client added

---
+++ client/src/Header.js
@@ -0,0 +1,47 @@
+import { useContext, useEffect} from "react";
+import {Link} from "react-router-dom";
+import { UserContext } from "./UserContext";
+
+
+export default function Header() {
+  const {setUserInfo,userInfo} = useContext(UserContext);
+  useEffect(() => {
+    fetch('http://localhost:3000/profile', {
+      credentials: 'include',
+    }).then(response => {
+      response.json().then(userInfo => {
+        setUserInfo(userInfo);
+      });
+    });
+  }, []);
+
+  function logout() {
+    fetch('http://localhost:3000/logout', {
+      credentials: 'include',
+      method: 'POST',
+    });
+    setUserInfo(null);
+  }
+
+  const username = userInfo?.username;
+
+  return (
+    <header>
+      <Link to="/" className="logo">Travel and Adventure Blogs</Link>
+      <nav>
+        {username && (
+          <>
+            <Link to='/create/' className="cp">Create new post</Link>
+            <a onClick={logout} className="cp">Logout ({username})</a>
+          </>
+        )}
+        {!username && (
+          <>
+            <Link to='/login'>Login</Link>
+            <Link to='/register'>Register</Link>
+          </>
+        )}
+      </nav>
+    </header>
+  );
+}
